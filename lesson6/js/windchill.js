8f726833fc2bd31ba4a46cb1129dc40e44d30cdf
annie127 <haw21002@byui.edu>
2021-10-22 23:18:46 +0800

    windchill update

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,8 +1,14 @@
 let temp = parseFloat(document.querySelector('#temp').textContent);
 let speed = parseFloat(document.querySelector('#speed').textContent);
 
+let windchill;
 if (temp <= 50 && speed > 3) {
-
-    let result = 35.74 + (0.6215 * temp) - (35.75 * (speed ** 0.16))+ (0.4275 * temp * (speed ** 0.16));
+    f = 35.74 + (0.6215 * temp) - (35.75 * (speed ** 0.16))+ (0.4275 * temp * (speed ** 0.16));
+    windchill = `${f.toFixed(1)} \u00B0F`;
 }
-document.querySelector('#wind').innerHTML = result.toFixed(1);
+
+else {
+    windchill = 'N/A';
+}
+
+document.querySelector('#wind').textContent = windchill;
