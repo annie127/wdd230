feec558318204354b60a76ecbcf04d54c5351951
annie127 <haw21002@byui.edu>
2021-10-30 18:41:05 +0800

    newest update

--- lesson7/js/visits.js
+++ lesson7/js/visits.js
@@ -5,7 +5,7 @@ const lastVisit = localStorage.getItem('lastvisit');
 let messageDays;
 if (lastVisit > 0 ) {
     daysBetweenVisits = (lastVisit - todaysDate) / millis;
-    messageDays = `Welcome back. It has been ${daysBetweenVisits.toFixed(0)} days since your last visit.`;
+    messageDays = `Welcome back. It has been ${daysBetweenVisits.toFixed(0)} day(s) since your last visit.`;
 }
 else {
     messageDays = "Welcome. This is your first visit. Enjoy our gallery!"
@@ -13,8 +13,6 @@ else {
 
 document.getElementById("visits").innerHTML = messageDays;
 
-localStorage.setItem('lastVisit', todaysDate);
-
-
+localStorage.setItem('lastvisit', todaysDate);
 
 
