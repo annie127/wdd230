22c3ce7f4cd46ea925787312ed85bdc67003121e
annie127 <haw21002@byui.edu>
2021-10-31 11:40:19 +0800

    newest update

--- lesson7/js/visits.js
+++ lesson7/js/visits.js
@@ -1,19 +1,19 @@
 const todaysDate = Date.now();
 const millis = 8640000;
-const lastVisit = localStorage.getItem('lastvisit');
+const prevDate = localStorage.getItem('lastVisit');
 
-daysBetweenVisits = (todaysDate - lastVisit) / millis;
+daysBetweenVisits = Math.floor((todaysDate - prevDate) / millis);
 
 let messageDays;
 if (daysBetweenVisits >= 1) {
-    messageDays = `Welcome back. It has been ${daysBetweenVisits.toFixed(0)} day(s) since your last visit.`;
+    messageDays = `Welcome back. It has been ${daysBetweenVisits} day(s) since your last visit.`;
 }
 else {
     messageDays = "Welcome. This is your first visit. Enjoy our gallery!"
 }
 
-document.getElementById("visits").innerHTML = messageDays;
+document.getElementById("visits").innerHTML = todaysDate;
 
-localStorage.setItem('lastvisit', todaysDate);
+localStorage.setItem('lastVisit', todaysDate);
 
 
