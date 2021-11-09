a92cdccece60f2537651862a55c033bf9ef4cf56
annie127 <haw21002@byui.edu>
2021-11-09 10:04:03 +0800

    lesson 9 update

---
+++ lesson9/js/pancake.js
@@ -0,0 +1,7 @@
+let day = new Date().getDay();
+
+function setDiv() {
+    if (day == 5) {
+        document.getElementById("alert-banner").style.display = "block";
+ }
+}
---
+++ lesson9/js/windchill.js
@@ -0,0 +1,13 @@
+let temp = parseFloat(document.querySelector('#temp').textContent);
+let speed = parseFloat(document.querySelector('#speed').textContent);
+
+let windchill;
+if (temp <= 50 && speed > 3) {
+    f = 35.74 + (0.6215 * temp) - (35.75 * (speed ** 0.16))+ (0.4275 * temp * (speed ** 0.16));
+    windchill = `${f.toFixed(0)} \u00B0F`;
+}
+else {
+    windchill = 'N/A';
+}
+
+document.querySelector('#wind').textContent = windchill;
