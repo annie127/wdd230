0ce952548e897e8664564af4cabc8bdf0ff80db8
annie127 <haw21002@byui.edu>
2021-12-10 22:33:11 +0800

    json update

---
+++ final-project/js/listgrid.js
@@ -0,0 +1,25 @@
+var MyBtn = document.getElementsByClassName('mybtn');
+var index = 0;
+
+function Button(n) {
+    CurrentShowButton(index = n);
+}
+
+function CurrentShowButton(n) {
+    for (let i = 0; i < MyBtn.length; i++) {
+        MyBtn[i].className = MyBtn[i].className.replace(" Active", "");
+    }
+
+        MyBtn[n].className += " Active";
+}
+
+function List(){
+    let myCard = document.querySelector(".business-cards");
+    myCard.style.display = "block";
+}
+
+function Grid(){
+    let myCard = document.querySelector(".business-cards");
+    myCard.style.display = "grid";
+
+}
